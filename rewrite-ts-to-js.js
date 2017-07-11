54d5fbbedb644a0ce027b3e886e6eab2cdf3914b
Jordan Last <jordan.michael.last@gmail.com>
2017-07-11 00:50:04 -0600

    rewriting paths, creating a dist directory

--- rewrite-ts-to-js.js
+++ rewrite-ts-to-js.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
-changeTsToJs('./', 'prendus-view-question', '.html');
-changeTsToJs('./', 'prendus-edit-question', '.html');
+changeTsToJs('./', './dist/', 'prendus-view-question', '.html');
+changeTsToJs('./', './dist/', 'prendus-edit-question', '.html');
 
-function changeTsToJs(filePath, fileName, fileExtension) {
-    const fileContents = fs.readFileSync(`${filePath}${fileName}${fileExtension}`);
-    fs.writeFileSync(`${filePath}${fileName}${fileExtension}`, fileContents.toString().replace(`<script src="${fileName}.ts"></script>`, `<script src="${fileName}.js"></script>`));
+function changeTsToJs(sourceFilePath, destinationFilePath, fileName, fileExtension) {
+    const fileContents = fs.readFileSync(`${sourceFilePath}${fileName}${fileExtension}`);
+    fs.writeFileSync(`${destinationFilePath}${fileName}${fileExtension}`, fileContents.toString().replace(`<script src="${fileName}.ts"></script>`, `<script src="${fileName}.js"></script>`));
 }
